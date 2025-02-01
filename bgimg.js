a1cf826d6007d3289c6a3a9e7a76559b30dcda94
msupritha512 <msupritha512@gmail.com>
2025-02-01 14:27:45 +0530

    Delete bgimg.js

--- bgimg.js
+++
@@ -1,9 +0,0 @@
-let images = document.getElementsByTagName("img");
-for(let i=0; i< images.length; i++) {
-    images[i].addEventListener('click',()=> {
-        document.querySelector('body').style.cssText = `
-        background-image:url("${images[i].src}");
-        background-repeat: no-repeat;
-        background-size: cover;`;
-    });
-}
